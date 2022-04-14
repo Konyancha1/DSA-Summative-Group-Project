648b52b8a07e6964f6f2ef616bd91b4ff1b6e6f8
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2022-04-14 14:05:34 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,16 +1,16 @@
 from tkinter import *
 from tkinter import ttk
-import os
+from tkinter import messagebox
 
 root = Tk()
 root.title("E-commerce Order App")
 root.geometry("1280x720")
 
 title = Label(root, text='Online Shirt Order App', bg='white', fg='black',
-              font=('Montserrat', 35, 'bold'), relief=GROOVE, bd=12)
+              font=('Montserrat', 30, 'bold'), relief=GROOVE, bd=12)
 title.pack(fill=X)
 
-Frame_1 = Frame(root, bg="white", relief=RIDGE, bd=10)
+Frame_1 = Frame(root, relief=RIDGE, bd=10, bg='black')
 Frame_1.place(x=10, y=80, width=650, height=530)
 
 Frame_2 = Frame(root, bg="white", relief=RIDGE, bd=10)
@@ -21,34 +21,35 @@ address = StringVar()
 email = StringVar()
 shirt = StringVar()
 size = StringVar()
+number = IntVar()
 
-lbl_1 = Label(Frame_1, text="Enter Full name:", font=('Montserrat', 20, 'bold'), fg='black',
-              bg='white')
+lbl_1 = Label(Frame_1, text="Enter Full name:", font=('Montserrat', 15, 'bold'), fg='white',
+              bg='black')
 lbl_1.grid(row=0, column=0, padx=30, pady=10)
 txt_1 = Entry(Frame_1, font=('Montserrat', 15, 'bold'), relief=RIDGE, bd=7, textvariable=name)
 txt_1.grid(row=0, column=1, pady=10, sticky='w')
 
-lbl_2 = Label(Frame_1, text="Enter Your Address:", font=('Montserrat', 20, 'bold'), fg='black',
-              bg='white')
+lbl_2 = Label(Frame_1, text="Enter Your Address:", font=('Montserrat', 15, 'bold'), fg='white',
+              bg='black')
 lbl_2.grid(row=1, column=0, padx=30, pady=10)
 txt_2 = Entry(Frame_1, font=('Montserrat', 15, 'bold'), relief=RIDGE, bd=7, textvariable=address)
 txt_2.grid(row=1, column=1, pady=10, sticky='w')
 
-lbl_3 = Label(Frame_1, text="Enter Your Email:", font=('Montserrat', 20, 'bold'), fg='black',
-              bg='white')
+lbl_3 = Label(Frame_1, text="Enter Your Email:", font=('Montserrat', 15, 'bold'), fg='white',
+              bg='black')
 lbl_3.grid(row=2, column=0, padx=30, pady=10)
 txt_3 = Entry(Frame_1, font=('Montserrat', 15, 'bold'), relief=RIDGE, bd=7, textvariable=email)
 txt_3.grid(row=2, column=1, pady=10, sticky='w')
 
-lbl_4 = Label(Frame_1, text="Choose Shirt Type:", font=('Montserrat', 20, 'bold'), fg='black',
-              bg='white')
+lbl_4 = Label(Frame_1, text="Choose Shirt Type:", font=('Montserrat', 15, 'bold'), fg='white',
+              bg='black')
 lbl_4.grid(row=3, column=0, padx=30, pady=10)
 combo_1 = ttk.Combobox(Frame_1, font=('Montserrat', 15), state='readonly', textvariable=shirt)
 combo_1['value'] = ('Plain', 'Floral', 'Short Sleeve Shirt', 'Flannel Shirt', 'Dress Shirt')
 combo_1.grid(row=3, column=1, pady=10)
 
-lbl_5 = Label(Frame_1, text="Choose Your Size:", font=('Montserrat', 20, 'bold'), fg='black',
-              bg='white')
+lbl_5 = Label(Frame_1, text="Choose Your Size:", font=('Montserrat', 15, 'bold'), fg='white',
+              bg='black')
 lbl_5.grid(row=4, column=0, padx=30, pady=10)
 combo_2 = ttk.Combobox(Frame_1, font=('Montserrat', 15), state='readonly', textvariable=size)
 combo_2['value'] = ('X-Large', 'Large', 'Medium', 'Small', 'X-Small')
@@ -67,6 +68,47 @@ def output():
     out_3.config(text='Customer Contact: ' + str(user_email))
     out_4.config(text='Shirt Type: ' + str(shirt_type))
     out_5.config(text='Shirt Size: ' + str(shirt_size))
+
+    check_button = Button(Frame_3, text='Check Order', font=('Arial', 15, 'bold'), bg='yellow',
+                          fg='black', width=20, command=check)
+    check_button.grid(row=0, column=1, padx=130, pady=15)
+
+    txt_file = open("order.txt", "w")
+    txt_file.write("Customer name: " + str(user))
+    txt_file.write("\nCustomer Address: " + str(user_address))
+    txt_file.write("\nCustomer Contact: " + str(user_email))
+    txt_file.write("\nShirt Type: " + str(shirt_type))
+    txt_file.write("\nShirt Size: " + str(shirt_size))
+    txt_file.close()
+
+
+def ask():
+    ans = messagebox.askquestion("Confirm Exit", "Do you really want to exit?")
+    if ans == "yes":
+        root.quit()
+
+
+def check():
+    Frame_4 = Frame(root, bg="black", relief=RIDGE, bd=10)
+    Frame_4.place(x=10, y=80, width=1260, height=530)
+    lbl = Label(Frame_4, text="Enter Order Number:", font=('Montserrat', 15, 'bold'), fg='white',
+                bg='black')
+    lbl.grid(row=0, column=0, padx=30, pady=10)
+    txt = Entry(Frame_4, font=('Montserrat', 15, 'bold'), relief=RIDGE, bd=7, textvariable=number)
+    txt.grid(row=0, column=1, pady=10, sticky='w')
+    enter = Button(Frame_4, text='Submit', font=('Arial', 12, 'bold'), bg='yellow',
+                   fg='black', command=check)
+    enter.grid(row=0, column=2, padx=10, pady=15)
+
+    def back():
+        Frame_1.tkraise()
+        Frame_2.tkraise()
+        Frame_4.destroy()
+
+    back = Button(Frame_3, text='Go Back', font=('Arial', 15, 'bold'), bg='yellow',
+                  fg='black', command=back, width=20)
+    back.grid(row=0, column=2, padx=10, pady=15)
+    Frame_4.tkraise()
 
 
 out = Label(Frame_2, font=('Montserrat', 20, 'bold'), fg='black',
@@ -95,10 +137,8 @@ button_1 = Button(Frame_1, text='Submit Order', font=('Arial', 15, 'bold'), bg='
                   command=output)
 button_1.grid(row=5, column=1, pady=10)
 
-button_2 = Button(Frame_3, text='Add Order', font=('Arial', 15, 'bold'), bg='yellow', fg='black', width=10)
-button_2.grid(row=0, column=1, padx=250, pady=15)
-
-button_3 = Button(Frame_3, text='Exit', font=('Arial', 15, 'bold'), bg='yellow', fg='black', width=10)
-button_3.grid(row=0, column=3, padx=230, pady=15)
+button_2 = Button(Frame_3, text='Exit', font=('Arial', 15, 'bold'), bg='yellow', fg='black', width=20,
+                  command=ask)
+button_2.grid(row=0, column=3, padx=130, pady=15)
 
 root.mainloop()
